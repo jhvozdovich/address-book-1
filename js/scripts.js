e0094f1983b488b9f9283ce672be4a8430c791e8
mai cook <mailiecook@yahoo.com>
2020-04-06 10:03:53 -0700

    add document.ready(function() in scripts.js file, add variables firstName,lastName, and phoneNumber

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,13 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var firstName = $("input#firstName").val();
+    var lastName = $("input#lastName").val();
+    var phoneNumber = $("input#number").val();
+    console.log(firstName,lastName,phoneNumber);
+  });
+});
