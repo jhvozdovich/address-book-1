8ec2f91321b8bb02635f5774d6f26a791cc135b7
mai cook <mailiecook@yahoo.com>
2020-04-06 10:49:29 -0700

    add contact object, and prototype fullName

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
+//------Buiseness Logic for Address book-----
+
+//-------Business Logic for Contact
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
-
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-
+//-------UI Logic-------
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var firstName = $("input#firstName").val();
     var lastName = $("input#lastName").val();
     var phoneNumber = $("input#number").val();
-    Contact(firstName, lastName, phoneNumber);
-    console.log(C);
+    var newContact = new Contact(firstName, lastName, phoneNumber);
+    console.log(newContact.fullName());
   });
 });
